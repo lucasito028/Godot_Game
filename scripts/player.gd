9247db633e53d0b79b5c182573a30621d01e9066
lucasito028 <lucastoshihiroito28@gmail.com>
2024-11-14 17:37:18 -0300

    Sua mensagem de commit explicando as mudanças

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var speed = 100
 @export var jump_veocity = -300
 
+@onready var ani_sprite = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
@@ -13,6 +15,20 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_veocity
+	
+	if direction > 0:
+		ani_sprite.flip_h = false
+	elif direction < 0:
+		ani_sprite.flip_h = true
+		
+	if is_on_floor():
+		if direction == 0:
+			ani_sprite.play("default")
+		else:
+			ani_sprite.play("run")
+	
+	else: 
+		ani_sprite.play("jump")
 	
 	if direction:
 		velocity.x = direction * speed
